fa0ba32e3b8c19c5841846ecaef1074656fbcd05
佟舟 <437753880@qq.com>
2021-05-27 17:34:59 +0800

    1

--- src/layouts/BasicLayout/index.jsx
+++ src/layouts/BasicLayout/index.jsx
@@ -4,7 +4,7 @@ import CustomHeader from '@/components/common/CustomHeader'
 import Side from '@/components/common/Side'
 import styles from './index.less'
 
-const a =2
+const a =2222
 
 function Index(props) {
   return (
