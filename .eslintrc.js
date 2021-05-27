2992a92c69fbd1fbc372b61f5879928c87057a9d
佟舟 <437753880@qq.com>
2021-05-27 18:28:28 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-    extends: [require.resolve('@umijs/fabric/dist/eslint')],
-    globals: {
-      ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
-      page: true,
-      REACT_APP_ENV: true,
-    },
-    rules: {
-      'prefer-promise-reject-errors': 0, // reject里只能放new Error
-      'prefer-destructuring': 0, // 必须使用解构
-      'no-use-before-define': ['error', { functions: false }], // 声明之前不能使用，除了function变量提升
-      radix: 0, // parseInt
-      'global-require': 0,
-      'no-restricted-syntax': 0, // for in
-      'no-underscore-dangle': 0, // 不能使用下划线 _apply
-    },
-  };
-  
+	env: {
+		node: true,
+		browser: true,
+		commonjs: true,
+		amd: true
+	},
+	extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: "script"
+	},
+	plugins: ["react", "@typescript-eslint"],
+	rules: {}
+};
--- src/layouts/BasicLayout/index.jsx
+++ src/layouts/BasicLayout/index.jsx
@@ -1,23 +1,19 @@
-import React, { Fragment } from 'react'
-import { renderChildren } from '@/router'
-import CustomHeader from '@/components/common/CustomHeader'
-import Side from '@/components/common/Side'
-import styles from './index.less'
-
-const a =2222222111
+import React, { Fragment } from "react";
+import { renderChildren } from "@/router";
+import CustomHeader from "@/components/common/CustomHeader";
+import Side from "@/components/common/Side";
+import styles from "./index.less";
 
 function Index(props) {
-  return (
-    <Fragment>
-      <CustomHeader />
-      <div className={styles.main_content}>
-        <Side />
-        <div className={styles.page_content}>
-          {renderChildren(props)}
-        </div>
-      </div>
-    </Fragment>
-  )
+	return (
+		<Fragment>
+			<CustomHeader />
+			<div className={styles.main_content}>
+				<Side />
+				<div className={styles.page_content}>{renderChildren(props)}</div>
+			</div>
+		</Fragment>
+	);
 }
 
-export default Index
+export default Index;
